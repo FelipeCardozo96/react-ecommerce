a103d4185ab0a97698a999dce12d47d1e4152027
FelipeCardozo96 <felipemcr996@gmail.com>
2023-07-06 13:31:44 -0300

    Se agregó input de búsqueda y su lógica de filtrado por caracteres. Además de linkear los elementos del navbar junto con las categorías. Se estilizó el checkout de las ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,15 +1,21 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
 
   return (
-    <div className="flex justify-between items-center mb-3 border border-black">
-      <p>
+    <div className='flex justify-between items-center mb-3 border border-black p-4 w-80 rounded-lg'>
+      <div className='flex justify-between w-full'>
+        <p className='flex flex-col'>
         <span>01.02.23</span>
-        <span>{totalProducts}</span>
-        <span>{totalPrice}</span>
-      </p>
+        <span>{totalProducts} articles</span>
+        </p>
+        <p className='flex items-center gap-2'>
+        <span className='font-medium text-xl'>${totalPrice}</span>
+        <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer' />
+        </p>
+
+      </div>
     </div>
   )
 }
