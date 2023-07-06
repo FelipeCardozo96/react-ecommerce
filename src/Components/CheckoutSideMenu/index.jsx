fa35d4407613b007091dfd60709aaed4ca45b960
FelipeCardozo96 <felipemcr996@gmail.com>
2023-07-06 11:16:16 -0300

    Se linkeó el checkout de la orden para llevar a otra web donde se encuentren todos los items del carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../OrderCard'
@@ -13,6 +14,19 @@ const CheckoutSideMenu = () => {
     context.setCartProducts(filteredProducts)
   }
 
+  const handleCheckout = () => {
+    const orderToAdd = {
+      date: '01.02.2023',
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts)
+    }
+
+    context.setOrder([...context.order, orderToAdd])
+    context.setCartProducts([])
+    context.setCount(0)
+  }
+
   return (
     <aside
       className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -24,7 +38,7 @@ const CheckoutSideMenu = () => {
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
       </div>
-      <div className='px-4 overflow-y-scroll'>
+      <div className='px-4 overflow-y-scroll flex-1'>
       {
         context.cartProducts.map(product => (
           <OrderCard 
@@ -43,6 +57,10 @@ const CheckoutSideMenu = () => {
         <span className='text-md'>Total :</span>
         <span className='font-medium text-xl'>{totalPrice(context.cartProducts)}</span>
       </p>
+      <Link to='my-orders/last'>
+        <button className='w-full bg-black p-3 text-white rounded-lg mt-2 mb-6'
+        onClick={() => handleCheckout()}>Checkout</button>
+      </Link>
       </div>
     </aside>
   )
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,7 +1,15 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
-const index = props => {
+const OrderCard = props => {
     const { id, title, imageURL, price, handleDelete} = props
+    let renderXMarkIcon
+    if (handleDelete) {
+        renderXMarkIcon = <XMarkIcon
+        className='h-6 w-6 text-black cursor-pointer'
+        onClick={() => handleDelete(id)}>
+        </XMarkIcon>
+    }
+
   return (
     <div className='flex justify-between items-center mb-3'>
         <div className='flex items-center gap-2'>
@@ -12,13 +20,10 @@ const index = props => {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>${price}</p>
-                <XMarkIcon
-            className='h-6 w-6 text-black cursor-pointer'
-            onClick={() => handleDelete(id)}>
-            </XMarkIcon>
+                {renderXMarkIcon}
         </div>
     </div>
   )
 }
 
-export default index
+export default OrderCard
